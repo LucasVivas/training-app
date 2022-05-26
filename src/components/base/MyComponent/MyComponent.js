20b1f9efd932e03e0b16ec0933a16f8494261809
LucasVivas <lucas.vivas33@gmail.com>
2022-05-26 23:44:03 +0200

    add first component

---
+++ src/components/base/MyComponent/MyComponent.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './MyComponent.css';
+
+const MyComponent = () => (
+  <div className="MyComponent" data-testid="MyComponent">
+    MyComponent Component
+  </div>
+);
+
+MyComponent.propTypes = {};
+
+MyComponent.defaultProps = {};
+
+export default MyComponent;
