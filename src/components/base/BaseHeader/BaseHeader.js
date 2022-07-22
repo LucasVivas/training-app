abbe5c3512b63279f218e5976f4cd8e30df570f2
LucasVivas <lucas.vivas33@gmail.com>
2022-07-22 17:20:11 +0200

    add material ui

--- src/components/base/BaseHeader/BaseHeader.js
+++ src/components/base/BaseHeader/BaseHeader.js
@@ -1,11 +1,32 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import './BaseHeader.css';
 
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+
 const BaseHeader = () => (
-  <div className="BaseHeader" data-testid="BaseHeader">
-    BaseHeader Component
-  </div>
+  <Box sx={{ flexGrow: 1 }}>
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton
+          size="large"
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          sx={{ mr: 2 }}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          Training App
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  </Box>
 );
 
 BaseHeader.propTypes = {};
