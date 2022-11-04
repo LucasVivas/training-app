9309d041bb709ffaf541c2b7685c62fa87bafc40
LucasVivas <lucas.vivas33@gmail.com>
2022-11-04 16:44:29 +0100

    add menu bar

--- TODO.md
+++ TODO.md
@@ -1,5 +1,8 @@
-- Add redux
+Functionnality:
 - Create exercice from front 
+
+Tech
+- Add redux
 - Create some tests jest
 - Create some tests stories
 - Check how to really use lazy react
--- src/components/base/BaseHeader/BaseHeader.jsx
+++ src/components/base/BaseHeader/BaseHeader.jsx
@@ -1,11 +1,33 @@
 import React from 'react';
 import './BaseHeader.css';
 
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
 function BaseHeader() {
   return (
-    <div className="BaseHeader" data-testid="BaseHeader">
-      <h1>Training App</h1>
-    </div>
+    <Box sx={{ flexGrow: 1 }} className="BaseHeader" data-testid="BaseHeader">
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Training App
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 }
 
